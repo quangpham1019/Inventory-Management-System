cfd42f39524b1642376db926875c9a6a3d0fe881
quangpham1019 <quangpham1019@gmail.com>
2023-10-18 23:36:54 -0500

    - add save order with order items functionality - changes are reflected in databases;

--- src/main/java/com/example/demo/domain/Product.java
+++ src/main/java/com/example/demo/domain/Product.java
@@ -22,6 +22,7 @@ import java.util.Set;
 public class Product implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+            @Column(name = "product_id")
     long id;
     String name;
     @Min(value = 0, message = "Price value must be positive")
