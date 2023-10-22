e8c4a31c1c7efaa8235a7617c407c6f9c6e4badc
quangpham1019 <quangpham1019@gmail.com>
2023-10-22 18:30:11 -0500

    - clean up;

--- src/main/java/com/example/demo/repositories/ItemTangibleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repositories;
-
-import com.example.demo.domain.ItemTangible;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ItemTangibleRepository extends CrudRepository<ItemTangible, Long> {
-}
