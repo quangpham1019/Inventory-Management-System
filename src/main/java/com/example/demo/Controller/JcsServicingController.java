5b5a09051bb1c1f9c38ee910520c721e8c60c9fe
Quang Pham_ MSI <quangpham1019@gmail.com>
2025-01-06 14:14:09 -0800

    refactor inventory controller.

---
+++ src/main/java/com/example/demo/Controller/JcsServicingController.java
@@ -0,0 +1,60 @@
+package com.example.demo.Controller;
+
+import com.example.demo.Domain.JcsServicing;
+import com.example.demo.Domain.Order;
+import com.example.demo.Service.Interface.JcsServicingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/api/v1/jcsServicing")
+@RequiredArgsConstructor
+public class JcsServicingController {
+
+    private final JcsServicingService jcsServicingService;
+    private final Order order;
+
+    @GetMapping("/addJcsServicing")
+    public String getAddServiceForm(Model model) {
+        model.addAttribute("jcsServicing", new JcsServicing());
+        model.addAttribute("action", "add");
+        return "form/service_form";
+    }
+    @PostMapping("/processJcsServicing")
+    public String processNewService(@ModelAttribute(name = "jcsServicing") JcsServicing newJcsServicing) {
+        jcsServicingService.save(newJcsServicing);
+        return "redirect:/";
+    }
+    @GetMapping("/updateJcsServicing")
+    public String getUpdateService(@RequestParam int serviceId, Model model) {
+
+        JcsServicing updateJcsServicing = jcsServicingService.findById(serviceId);
+        model.addAttribute("jcsServicing", updateJcsServicing);
+        model.addAttribute("action", "update");
+        return "form/service_form";
+    }
+    @PostMapping("/updateJcsServicing")
+    public String updateJcsServicingProcess(@RequestParam int serviceId,
+                                            @ModelAttribute("jcsServicing") JcsServicing updateJcsServicing) {
+
+        JcsServicing jcsServicing = jcsServicingService.findById(serviceId);
+        jcsServicing.setDuration(updateJcsServicing.getDuration());
+        jcsServicing.setName(updateJcsServicing.getName());
+        jcsServicing.setPrice(updateJcsServicing.getPrice());
+        jcsServicingService.save(jcsServicing);
+        return "redirect:/";
+    }
+    @GetMapping("/deleteJcsServicing")
+    public String deleteJcsServicing(@RequestParam int serviceId) {
+        JcsServicing jcsServicing = jcsServicingService.findById(serviceId);
+        if (order.getOrderItemSet()
+                .stream()
+                .anyMatch(orderItem -> orderItem.getItem().equals(jcsServicing))) {
+            return "error/error_item_in_order";
+        }
+        jcsServicingService.deleteById(serviceId);
+        return "redirect:/";
+    }
+}
