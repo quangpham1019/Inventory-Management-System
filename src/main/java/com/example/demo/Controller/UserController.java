b5fb12a39c8c8d63ad7cd7b820340170e38c5004
Quang Pham_ MSI <quangpham1019@gmail.com>
2025-01-06 14:00:57 -0800

    refactor admin controller to user controller.

--- src/main/java/com/example/demo/Controller/UserController.java
+++ src/main/java/com/example/demo/Controller/UserController.java
@@ -1,59 +1,94 @@
 package com.example.demo.Controller;
 
-import com.example.demo.Domain.*;
 import com.example.demo.Security.AppUser;
-import com.example.demo.Service.Interface.CustomerService;
-import com.example.demo.Service.Interface.ItemService;
-import com.example.demo.Service.Interface.OrderService;
-import com.example.demo.Service.Interface.ProductService;
-import com.example.demo.Service.Interface.ReportService;
+import com.example.demo.Security.AppUserService;
+import com.example.demo.Security.LoginProvider;
+import com.example.demo.Domain.UserEntity;
+import com.example.demo.Repository.CRUDRepository.UserEntityRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import jakarta.transaction.Transactional;
-import jakarta.validation.Valid;
 import java.util.List;
-import java.util.stream.Collectors;
 
-// TODO: separate UserController into CustomerController, SalesController, ReportController
-
+// TODO: Refactor UserController into ReportController, UserController
+@PreAuthorize("hasAuthority('ADMIN')")
 @Controller
 @RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
 public class UserController {
 
-    @Valid
-    private Order order;
-    private ItemService itemService;
-    private ProductService productService;
-    private OrderService orderService;
-    private ReportService reportService;
-    private CustomerService customerService;
-    private AppUser appUser;
+    private final UserEntityRepository userEntityRepository;
+    private final AppUserService appUserService;
 
-    @ModelAttribute
-    AppUser appUser(@AuthenticationPrincipal AppUser appUser) {
-        this.appUser = appUser;
-        return appUser;
+    @GetMapping("/manageUsers")
+    public String getManageUsersPage(Model model) {
+        List<UserEntity> users = (List<UserEntity>)userEntityRepository.findAll();
+        model.addAttribute("users", users);
+        return "menu pages/users";
     }
 
-    @Autowired
-    public UserController(ItemService itemService, ProductService productService, Order order, OrderService orderService, ReportService reportService, CustomerService customerService) {
-        this.itemService = itemService;
-        this.productService = productService;
-        this.order = order;
-        this.orderService = orderService;
-        this.reportService = reportService;
-        this.customerService = customerService;
+    @GetMapping("/addUser")
+    public String getNewUserForm(Model model,
+                                 @ModelAttribute("error") Exception e) {
+        model.addAttribute("updateUser", new UserEntity());
+        model.addAttribute("oldUsername", "");
+        model.addAttribute("action", "add");
+        model.addAttribute("error", e);
+        return "form/user_form";
     }
 
+    @PostMapping("/processUser")
+    public String processNewEmployee(@ModelAttribute(name = "user") UserEntity newUser) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        var userToAdd = AppUser.builder()
+                .username(newUser.getUsername())
+                .name(newUser.getName())
+                .email(newUser.getEmail())
+                .provider(LoginProvider.APP)
+                .password(passwordEncoder.encode(newUser.getPassword()))
+                .authorities(List.of(new SimpleGrantedAuthority("USER")))
+                .build();
 
+        appUserService.createUser(userToAdd);
+        return "confirmation/user_add_success";
+    }
 
+    @GetMapping("/updateUser")
+    public String getUpdateUser(@RequestParam String username, Model model) {
 
+        UserEntity user = userEntityRepository.findById(username).orElse(null);
+        model.addAttribute("updateUser", user);
+        model.addAttribute("oldUsername", user.getUsername());
+        model.addAttribute("action", "update");
+        return "form/user_form";
+    }
+
+    @PostMapping("/updateUser")
+    public String updateUserProcess(@RequestParam String USERNAME,
+                                    @ModelAttribute("updateUser") UserEntity updateUser,
+                                    Model model,
+                                    Errors errors) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("action", "update");
+            return "form/user_form";
+        }
+
+        updateUser.setUsername(USERNAME);
+        appUserService.updateUser(updateUser);
+
+        return "redirect:/api/v1/user/manageUsers";
+    }
+
+    @GetMapping("/deleteUser")
+    public String deleteUser(@RequestParam String username) {
+        appUserService.deleteUser(username);
+        return "redirect:/api/v1/user/manageUsers";
+    }
 }
